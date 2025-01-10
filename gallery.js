245e767ccafd71230527fa415c12be847362f4f3
Sam-Hoeterickx <125275911+Sam-Hoeterickx@users.noreply.github.com>
2025-01-10 13:48:35 +0100

    Updated path

--- gallery.js
+++ gallery.js
@@ -16,8 +16,8 @@ function selectAllImages() {
             e.preventDefault();
 
             const imageUrl = e.currentTarget.src; 
-            const imagePath = imageUrl.split("5500/assets/images/Gallery/")[1];
-            // const imagePath = imageUrl.split("https://sam-hoeterickx.github.io/PlayPadelPortugal/")[1];
+            // const imagePath = imageUrl.split("5500/assets/images/Gallery/")[1];
+            const imagePath = imageUrl.split("https://sam-hoeterickx.github.io/PlayPadelPortugal/assets/images/Gallery/")[1];
 
             console.log("Image URL:", imageUrl);
             console.log("Image Path:", imagePath);
